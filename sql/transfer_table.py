8f9969773c8e8c0ece42e658e04e2b6bd61092ea
wujianji <wujianji@gigacloudtech.com>
2025-07-02 16:05:37 +0800

    表迁移脚本

--- sql/transfer_table.py
+++ sql/transfer_table.py
@@ -1,29 +1,29 @@
 import pandas as pd
 import sys
 from pathlib import Path
+from datetime import datetime
 
 sys.path.append(str(Path(__file__).parent.parent))
 from util.rwFile import rfile, wfile
 
 
-def generate_sql(rules, default_map, from_table):
-    sql_list = []
-    for row in from_table:
-        columns = []
-        values = []
-        for field_to, field_from in rules.items():
-            columns.append(field_to)
-            values.append(escape(row[field_from]))
-        for field_to, default_value in default_map.items():
-            if field_to in columns:
+def generate_sql(table_name, mappings, defaults, table):
+    insert_sql_list = []
+    for row in table:
+        columns, values = [], []
+        for field, map_func in mappings.items():
+            columns.append(field)
+            values.append(escape(map_func(row)))
+        for field, default_value in defaults.items():
+            if field in columns:
                 continue
-            columns.append(field_to)
+            columns.append(field)
             values.append(escape(default_value))
         column_part = ",".join(columns)
         value_part = ",".join(values)
-        sql = f"INSERT INTO sys_monitor_config ({column_part}) VALUES ({value_part})"
-        sql_list.append(sql)
-    return sql_list
+        sql = f"INSERT INTO {table_name} ({column_part}) VALUES ({value_part})"
+        insert_sql_list.append(sql)
+    return insert_sql_list
 
 
 def escape(value):
@@ -34,22 +34,47 @@ def escape(value):
     return value
 
 
-rules = {
-    "monitor_table_sql": "tsql",
-    "monitor_subject": "subject",
-    "monitor_config_code": "subject",
-    "monitor_to": "tobox",
-    "monitor_cc": "ccbox",
+def concat_fields(row):
+    fields = [f"field{i}" for i in range(1, 41)]
+    result = ",".join(
+        row[field] for field in fields if row[field] is not None and row[field] != ""
+    )
+    return result
+
+
+def datetime_to_cron(row):
+    date_format = "%Y-%m-%d %H:%M:%S.%f"
+    dt = datetime.strptime(row["NextExecuteDate"], date_format)
+
+    cron = None
+    cycle_time = row["cycleTime"]
+    if cycle_time == 1 or cycle_time == "1":
+        cron = f"{dt.minute} * * * *"
+    elif cycle_time == 24 or cycle_time == "24":
+        cron = f"{dt.minute} {dt.hour} * * *"
+    elif cycle_time == 8 or cycle_time == "8":
+        cron = f"{dt.minute} 4,12,20 * * *"
+    elif cycle_time == 6 or cycle_time == "6":
+        cron = f"{dt.minute} 2,8,14,20 * * *"
+    else:
+        raise ValueError(f"Unknown cycleTime: {cycle_time} type: {type(cycle_time)}")
+    return cron
+
+
+mappings = {
+    "monitor_table_sql": lambda row: row["tsql"],
+    "monitor_subject": lambda row: row["subject"],
+    "monitor_to": lambda row: row["tobox"],
+    "monitor_cc": lambda row: row["ccbox"],
+    "monitor_table_field": concat_fields,
+    "cron": datetime_to_cron,
 }
 
-default_map = {"monitor_text_template": ""}
 
 from_table = rfile(r"tusTaskReminds.csv", type="csv")
-
-sql_list = generate_sql(rules, default_map, from_table)
-
+defaults = {"monitor_text_template": ""}
+sql_list = generate_sql("sys_monitor_config_1", mappings, defaults, from_table)
 wfile("result.sql", sql_list, type="list")
-
 
 print("from_table:", len(from_table))
 print("sql_list:", len(sql_list))
--- util/trim.py
+++ util/trim.py
@@ -1,12 +1,9 @@
 input_file = r'C:\Users\ADMIN\Desktop\temp\temp.txt'
 
 lines = ''
-with open(input_file, 'r', encoding='utf-8') as file:
+with open(input_file, 'rw', encoding='utf-8') as file:
     lines = file.readlines()
-
-trimmedLines = ['\'' + line.strip() + '\',' for line in lines]
-
-with open(input_file, 'w', encoding='utf-8') as file:
+    trimmedLines = ['\'' + line.strip() + '\',' for line in lines]
     file.writelines('\n'.join(trimmedLines))
 
 print("done:", input_file)
